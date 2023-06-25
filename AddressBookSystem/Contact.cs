f7273fd8fc563feb843bb0bc70b36ec99e87d46c
“rohyapawar2912” <rohitpawar8530@gmail.com>
2023-06-25 12:40:36 +0530

    [Rohit]Add: // add all files

---
+++ AddressBookSystem/Contact.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public void DisplayContactInfo()
+        {
+            Console.WriteLine("Contact Information:");
+            Console.WriteLine("First Name: " + FirstName);
+            Console.WriteLine("Last Name: " + LastName);
+            Console.WriteLine("Address: " + Address);
+            Console.WriteLine("City: " + City);
+            Console.WriteLine("State: " + State);
+            Console.WriteLine("Zip: " + Zip);
+            Console.WriteLine("Phone Number: " + PhoneNumber);
+            Console.WriteLine("Email: " + Email);
+        }
+    }
+}
---
+++ AddressBookSystem/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Contact contact = new Contact();
+
+            Console.WriteLine("Enter First Name:");
+            contact.FirstName = Console.ReadLine();
+
+            Console.WriteLine("Enter Last Name:");
+            contact.LastName = Console.ReadLine();
+
+            Console.WriteLine("Enter Address:");
+            contact.Address = Console.ReadLine();
+
+            Console.WriteLine("Enter City:");
+            contact.City = Console.ReadLine();
+
+            Console.WriteLine("Enter State:");
+            contact.State = Console.ReadLine();
+
+            Console.WriteLine("Enter Zip:");
+            contact.Zip = Console.ReadLine();
+
+            Console.WriteLine("Enter Phone Number:");
+            contact.PhoneNumber = Console.ReadLine();
+
+            Console.WriteLine("Enter Email:");
+            contact.Email = Console.ReadLine();
+
+            Console.WriteLine();
+            contact.DisplayContactInfo();
+        }
+    }
+}
